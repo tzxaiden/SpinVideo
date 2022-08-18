28014bf268fb3b32f8d18941dce4882a2a1974aa
Aidyn S <zauresalimbaeva3@gmail.com>
2022-08-18 15:04:32 -0500

    Handling Auththentication with Google Api Auth

--- streams/client/src/components/Header.js
+++ streams/client/src/components/Header.js
@@ -1,5 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import GoogleAuth from "./GoogleAuth";
+
+
+
 const Header = () => {
   return (
     <div className="ui secondary pointing menu">
@@ -10,9 +14,13 @@ const Header = () => {
         <Link to="/" className="item">
           All Streams
         </Link>
+        <GoogleAuth />
       </div>
     </div>
   );
 };
 
 export default Header;
+
+
+
--- streams/client/src/index.js
+++ streams/client/src/index.js
@@ -1,11 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
 
 import App from "./components/App";
+import reducers from "./reducers";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware()));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
+    ,
   </React.StrictMode>
 );
